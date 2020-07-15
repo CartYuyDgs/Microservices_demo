d32bc6ed57ebf1e68f1e3bb401d7e34fed05a50d
于洋 <yy_dgs@126.com>
2020-07-15 22:13:27 +0800

    中间件优化

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,4 +1,4 @@
-package main
+package middleware
 
 import "context"
 
@@ -19,4 +19,18 @@ func Chain(outer Middleware, other ...Middleware) Middleware {
 
 func Use(m ...Middleware) {
 	userMiddleware = append(userMiddleware, m...)
+}
+
+func BuildServerMiddleware(handle MiddlewareFunc) (handChain MiddlewareFunc) {
+	var mids []Middleware
+	if len(userMiddleware) != 0 {
+		mids = append(mids, userMiddleware...)
+	}
+
+	if len(mids) > 0 {
+		m := Chain(mids[0], mids[1:]...)
+		return m(handle)
+	}
+
+	return handle
 }
